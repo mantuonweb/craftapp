22a5be9e55cab649a9bca6168536df9bd1df5955
mantuonweb <mantuonweb@hotmail.com>
2022-01-13 16:10:11 +0530

    sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'craftapp';
+  toggled = false;
+  toggleSideBar(){
+    this.toggled = !this.toggled;
+
+  }
 }
