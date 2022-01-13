1930aa8b4bbd05b208eee2bf2f2cc0368573cef3
mantuonweb <mantuonweb@hotmail.com>
2022-01-13 20:26:33 +0530

    plugins

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import * as Highcharts from "highcharts";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,8 +8,57 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'craftapp';
   toggled = false;
-  toggleSideBar(){
-    this.toggled = !this.toggled;
+  Highcharts: typeof Highcharts = Highcharts;
+  pointStart = Date.UTC(2022, 0, 1);
+  chartOptions: Highcharts.Options = {
+    title: {
+      text: 'Sales & Expenses'
+    },
+    legend: {
+      enabled: false,
+      floating: true,
+      verticalAlign: 'bottom',
+      align: 'right'
+    },
+    rangeSelector: {
+      selected: 1
+    },
+    xAxis: {
+      type: 'datetime',
+      tickInterval: 1000 * 3600 * 24 * 30 // 1 month
+    },
+    series: [{
+      type: 'line',
+      data: [{ x: Date.UTC(2014, 0, 1), y: 1 },
+      { x: Date.UTC(2014, 1, 1), y: 2 },
+      { x: Date.UTC(2014, 2, 1), y: 3 },
+      { x: Date.UTC(2014, 3, 1), y: 4 },
+      { x: Date.UTC(2014, 4, 1), y: 5 },
+      { x: Date.UTC(2014, 5, 1), y: 6 },
+      { x: Date.UTC(2014, 6, 1), y: 7 },
+      { x: Date.UTC(2014, 7, 1), y: 8 },
+      { x: Date.UTC(2014, 8, 1), y: 9 },
+      { x: Date.UTC(2014, 9, 1), y: 10 },
+      { x: Date.UTC(2014, 10, 1), y: 11 },
+      { x: Date.UTC(2014, 11, 1), y: 12 }]
+    }],
 
+    responsive: {
+      rules: [{
+        condition: {
+          maxWidth: 500
+        },
+        chartOptions: {
+          legend: {
+            layout: 'horizontal',
+            align: 'center',
+            verticalAlign: 'bottom'
+          }
+        }
+      }]
+    }
+  };
+  toggleSideBar() {
+    this.toggled = !this.toggled;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
+import { HighchartsChartModule } from 'highcharts-angular';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     AlertModule.forRoot(),
+    HighchartsChartModule,
     SharedModule
   ],
   providers: [],
