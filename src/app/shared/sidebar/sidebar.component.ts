6bfb24ff67454a1582e4876b63d306e06d4bcd1e
mantuonweb <mantuonweb@hotmail.com>
2022-01-18 17:58:10 +0530

    Configuration

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -9,11 +9,18 @@ export class SidebarComponent implements OnInit {
   @Input() toggled = false;
   @Output() onClose = new EventEmitter<boolean>()
   @Output() onClick = new EventEmitter();
+
+  routeList = [
+    { route: '/admin/dashboard', displayName: 'Dashboard', cssClass: 'bi-house' },
+    { route: '/admin/items', displayName: 'Items', cssClass: 'bi-person-circle' },
+    { route: '/admin/invoices', displayName: 'Invoices', cssClass: 'bi-file-earmark-spreadsheet' },
+    { route: '/admin/customers', displayName: 'Customers', cssClass: 'bi-person-circle' }
+  ]
   constructor() { }
 
   ngOnInit(): void {
   }
-  toggleSideBar(){
+  toggleSideBar() {
     this.onClose.emit(false);
   }
 }
