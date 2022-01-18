1493dbd6f1cb025450a9ad4cea82072c9f7ef80d
mantuonweb <mantuonweb@hotmail.com>
2022-01-18 17:45:58 +0530

    added more routes

--- src/app/pages/admin/admin-routing.module.ts
+++ src/app/pages/admin/admin-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 const routes: Routes = [{
   path: '', 
@@ -9,7 +8,9 @@ const routes: Routes = [{
   children: [
     { path: '',redirectTo: 'dashboard',pathMatch: 'full'},
     { path: 'dashboard', loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule) },
-    { path: 'customers', loadChildren: () => import('./pages/customers/customers.module').then(m => m.CustomersModule) }
+    { path: 'customers', loadChildren: () => import('./pages/customers/customers.module').then(m => m.CustomersModule) },
+    { path: 'items', loadChildren: () => import('./pages/items/items.module').then(m => m.ItemsModule) },
+    { path: 'invoices', loadChildren: () => import('./pages/invoices/invoices.module').then(m => m.InvoicesModule) }
   ]
 }]
  
@@ -19,3 +20,6 @@ const routes: Routes = [{
   exports: [RouterModule]
 })
 export class AdminRoutingModule { }
+
+
+//ng generate module pages/admin/pages/items --route items --module admin.module -d
