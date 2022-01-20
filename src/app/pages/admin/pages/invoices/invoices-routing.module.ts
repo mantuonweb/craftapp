1b013647882d98e899c27756b3459cc39d72cbe6
mantuonweb <mantuonweb@hotmail.com>
2022-01-20 15:48:46 +0530

    edit route

--- src/app/pages/admin/pages/invoices/invoices-routing.module.ts
+++ src/app/pages/admin/pages/invoices/invoices-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InvoicesComponent } from './invoices.component';
 
-const routes: Routes = [{ path: '', component: InvoicesComponent }, { path: 'add', loadChildren: () => import('./add-invoice/add-invoice.module').then(m => m.AddInvoiceModule) }];
+const routes: Routes = [
+  { path: '', component: InvoicesComponent }, 
+  { path: 'create', loadChildren: () => import('./add-invoice/add-invoice.module').then(m => m.AddInvoiceModule) },
+  { path: 'edit/:id', loadChildren: () => import('./add-invoice/add-invoice.module').then(m => m.AddInvoiceModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/pages/admin/pages/items/items-routing.module.ts
+++ src/app/pages/admin/pages/items/items-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ItemsComponent } from './items.component';
 
-const routes: Routes = [{ path: '', component: ItemsComponent }, { path: 'add', loadChildren: () => import('./add-items/add-items.module').then(m => m.AddItemsModule) }];
+const routes: Routes = [{ path: '', component: ItemsComponent }, 
+{ path: 'create', loadChildren: () => import('./add-items/add-items.module').then(m => m.AddItemsModule) },
+{ path: 'edit/:id', loadChildren: () => import('./add-items/add-items.module').then(m => m.AddItemsModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
