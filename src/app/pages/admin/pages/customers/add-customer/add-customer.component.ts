bd2c433e475475a545eeb5995c7f5e7a661de053
mantuonweb <mantuonweb@hotmail.com>
2022-01-22 11:45:47 +0530

    Reactive form added

--- src/app/pages/admin/pages/customers/add-customer/add-customer.component.ts
+++ src/app/pages/admin/pages/customers/add-customer/add-customer.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { COUNTRIES } from '../../../../../../app/constants/countries';
 import { CURRENCIES } from '../../../../../../app/constants/currency';
-
+import { FormBuilder,Validators,FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-add-customer',
   templateUrl: './add-customer.component.html',
@@ -10,12 +10,55 @@ import { CURRENCIES } from '../../../../../../app/constants/currency';
 export class AddCustomerComponent implements OnInit {
   countries = COUNTRIES;
   currencies = CURRENCIES; 
-  constructor() { }
+  customerForm = this.fb.group({
+    basicInformation:this.fb.group({
+      displayName:['',Validators.required],
+      primaryContactName:[''],
+      email:[''],
+      phone:[''],
+      primaryCurrency:['INR'],
+      website:[''],
+      prefix:['']
+    }),
+    billingAddress:this.fb.group({
+      name:[''],
+      country:['IN'],
+      state:[''],
+      city:[''],
+      address:[''],
+      phone:[''],
+      zip:['']
+    }),
+    shippingAddress:this.fb.group({
+      name:[''],
+      country:['IN'],
+      state:[''],
+      city:[''],
+      address:[''],
+      phone:[''],
+      zip:['']
+    }),
+    customfields:['']
+  });
+  get f() { 
+    return this.customerForm.controls;
+  }
+  get bif(){
+    const bi = <FormGroup>this.customerForm?.controls?.basicInformation;
+    return bi?.controls;
+  }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    console.log(this.countries,this.currencies)
+    // console.log(this.countries,this.currencies);
   }
   save(){
-
+    console.log(this.customerForm.value);
+  }
+  copyFromBilling(){
+    const billingAddress = this.customerForm?.value?.billingAddress;
+    this.customerForm.patchValue({
+      shippingAddress:{...billingAddress}
+    });
   }
 }
